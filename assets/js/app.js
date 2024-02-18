657958f129cfc5ba717873c610bcd3a08bfa7533
projecthanif <iamustapha213@gmail.com>
2024-02-18 09:56:19 +0100

    add a closing button for the event dialog

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const signUp1 = document.querySelector('#signUp1');
 const logIn1 = document.querySelector('#logIn1');
+const close = document.querySelector('#close');
+const close2 = document.querySelector('#close2');
 
 const signUp2 = document.querySelector('#signUp2');
 const logIn2 = document.querySelector('#logIn2');
@@ -9,6 +11,10 @@ logIn1.addEventListener('click', LoginToggle);
 
 signUp2.addEventListener('click', signUpToggle);
 logIn2.addEventListener('click', LoginToggle);
+
+
+close.addEventListener('click', closingModel);
+close2.addEventListener('click', closingModel);
 
 
 
@@ -25,4 +31,18 @@ function LoginToggle() {
     if (loginModel.classList.contains('hidden')) {
         loginModel.classList.remove('hidden')
     }
+}
+
+function closingModel() {
+    const signUpModel = document.querySelector('#signupModel');
+    const loginModel = document.querySelector('#loginModel');
+
+    if (!signUpModel.classList.contains('hidden')) {
+        signUpModel.classList.add('hidden');
+    }
+
+    if (!loginModel.classList.contains('hidden')) {
+        loginModel.classList.add('hidden');
+    }
+
 }
